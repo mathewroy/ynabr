ee95c50caeca4ed540b6afd849e40555ba6ef2b6
Mat on HOME15 <mathewroy@hotmail.com>
2018-12-01 23:45:03 -0500

    Created functional plot that shows spending by category (as of day of the month).

--- Monthly_Spending.R
+++ Monthly_Spending.R
@@ -29,13 +29,20 @@ plotMonthlyspending <- function(start,end,categories) {
   end_date_of_int <- as.Date(end)
   day_of_mont_of_int <- lubridate::day(end)
   #day_of_mont_of_int <- 30
-  categories_of_int <- categories
+  categories_of_int <-
+    grep(
+      paste0(categories, collapse = "|"),
+      unique(df_transactions$category_name),
+      value = TRUE,
+      ignore.case = TRUE
+    )
+  
   df_of_interest <- df_transactions %>%
     filter(
       date >= start_date_of_int,
       date <= end_date_of_int,
       dayofmonth <= day_of_mont_of_int,
-      str_detect(category_name, categories_of_int)
+      category_name %in% categories_of_int
     ) %>%
     group_by(category_name, yearmo) %>%
     summarize(activity = sum(amount))
@@ -46,20 +53,38 @@ plotMonthlyspending <- function(start,end,categories) {
   sdofinterest = sd (-1 * df_of_interest$activity / 1E3)
   
   ## Create plot
-  out <- ggplot(df_of_interest,
-         mapping = aes(x = yearmo, y = -1 * activity / 1E3)) +
-      geom_col() +
-      geom_hline(yintercept = meanofinterest - (2 * sdofinterest)) +
-      geom_hline(yintercept = meanofinterest - (1 * sdofinterest)) +
-      geom_hline(yintercept = medianofinterest) +
-      geom_hline(yintercept = meanofinterest + (1 * sdofinterest)) +
-      geom_hline(yintercept = meanofinterest + (2 * sdofinterest)) +
-      theme(axis.text.x = element_text(angle = 45)) +
-      scale_x_discrete(name = "Year-Month") +
-      scale_y_continuous(name = "Spending", labels = scales::dollar)
-
+  out <- 
+    ggplot(df_of_interest, mapping = aes(x = yearmo, y = -1 * activity / 1E3)) +
+    ggtitle(paste0("Monthly Spending: ", paste0(categories_of_int,collapse = ", "))) + 
+    geom_col() +
+    geom_hline(yintercept = meanofinterest - (1 * sdofinterest)) +
+    geom_hline(yintercept = medianofinterest) +
+    #geom_hline(yintercept = meanofinterest) +
+    geom_hline(yintercept = meanofinterest + (1 * sdofinterest)) +
+    geom_text(aes(x = min(yearmo),y = meanofinterest - (1 * sdofinterest), label = "-1 S.D.: ", vjust = -0.5)) +
+    geom_text(aes(x = min(yearmo),y = medianofinterest, label = "Median: ", vjust = -0.5)) +
+    #geom_text(aes(x = min(yearmo),y = meanofinterest, label = "Mean: ", vjust = -0.5)) +
+    geom_text(aes(x = min(yearmo),y = meanofinterest + (1 * sdofinterest), label = "+1 S.D.: ", vjust = -0.5)) +
+    theme(axis.text.x = element_text(angle = 45)) +
+    scale_x_discrete(name = "Year-Month") +
+    scale_y_continuous(name = "Spending",
+                       labels = scales::dollar,
+                       breaks = sort(
+                         c(
+                           seq(0, ceiling(max(-1 * df_of_interest$activity / 1E3) / 10) * 10, length.out = 2), 
+                           meanofinterest - (1 * sdofinterest),
+                           #meanofinterest,
+                           medianofinterest,
+                           meanofinterest + (1 * sdofinterest)
+                           )
+                         )
+                       )
   return(out)  
 }
+
 plotMonthlyspending(start = "2018-01-01",
                     end = "2018-11-30",
-                    categories = c("Fun Money"))
+                    categories = c("Gas"))
+
+
+?paste
